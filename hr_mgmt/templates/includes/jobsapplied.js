bfa5f29ec40bcbcbe661c7a2dd472f5314e258af
Gangadhar Kadam <mbauskar@gmail.com>
2015-12-04 19:28:16 +0530

    [Fixes] switched to bootstrap table and other fixes

--- hr_mgmt/templates/includes/jobsapplied.js
+++ hr_mgmt/templates/includes/jobsapplied.js
@@ -7,8 +7,9 @@ frappe.call({
             callback: function(r) {
                 if(r.message) {
                     //console.log(r.message);
-                    var $table1=$(document).find('#jobs')
-                    var h = "<table border='1' align='center' cellpadding='150' cellspacing='13' width='80%'><tr style='padding=0px;'><th>#</th><th>Job ID</th><th>Role</th><th>Job Title</th> <th>Location</th><th>Experince</th><th>Job Applied Date</th></tr></thead><tbody style='padding=0px;'>"
+                    // var $table1=$(document).find('#jobs')
+                    // var h = "<table border='1' align='center' cellpadding='150' cellspacing='13' width='80%'><tr style='padding=0px;'><th>#</th><th>Job ID</th><th>Role</th><th>Job Title</th> <th>Location</th><th>Experince</th><th>Job Applied Date</th></tr></thead><tbody style='padding=0px;'>"
+                    var h = "<thead><tr><th>#</th><th>Job ID</th><th>Role</th><th>Job Title</th> <th>Location</th><th>Experince</th><th>Job Applied Date</th></tr></thead><tbody>"
                     for (i=0;i<r.message.length;i++){
                         var j=i+1
                         //h += '<tr><td width="20%">'
